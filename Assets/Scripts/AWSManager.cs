671679258ba5a95f0d70fd0ad9c22d36d726bf2a
lou <dustyfingers01257@gmail.com>
2022-03-30 10:45:19 -0600

    project init

---
+++ Assets/Scripts/AWSManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Amazon;
+
+public class AWSManager : MonoBehaviour
+{
+
+    void Awake()
+    {
+        UnityInitializer.AttachToGameObject(this.gameObject);
+    }
+
+}
